6cdfd576ee007c30625f7df322b02e2739ce2fda
DouglasCBS <douglascesar12@gmail.com>
2023-01-09 17:32:49 -0300

    Create operate function
    this function chooses which math function created previously
    should be used based on the operator used

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ let multiply = function(a, b) {
     return a * b;
 };
 
+let operate = function(operator, number1, number2) {
+    switch (operator) {
+        case "+":
+            return add(number1, number2);
+        case "-":
+            return subtract(number1, number2);
+        case "/":
+            return divide(number1, number2);
+        case "*":
+            return multiply(number1, number2);
+        default:
+            console.log("Invalid operator.");
+    }
+};
+
+
